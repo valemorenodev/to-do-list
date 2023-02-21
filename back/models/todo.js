7d8ba705c30ea19a526ec52fbb9a1413b4379a5d
dianamorenodev <valentinamoreno2002@gmail.com>
2023-02-21 13:54:40 -0500

    separar back del front

--- back/models/todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const TodoSchema = new mongoose.Schema({
-  title: String,
-  completed: Boolean
-});
-
-module.exports = mongoose.model("Todo", TodoSchema)
--- to-do/src/apis/index.js
+++ to-do/src/apis/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3030/"
+  baseURL: "https://to-do-list-back-bice.vercel.app/"
 });
